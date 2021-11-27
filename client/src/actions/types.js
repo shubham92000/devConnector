10ac82d15aff882ee699f20434ef718d3b6a63f2
shubham92000 <sbhsingh616@gmail.com>
2021-11-27 14:43:29 +0530

    deleted

--- client/src/actions/types.js
+++
@@ -1,2 +0,0 @@
-export const GET_ERRORS = 'GET_ERRORS';
-export const GET_USER = 'GET_USER';
--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://shubham:shubham@cluster0.se0vg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  secretOrKey: 'secret'
-}
