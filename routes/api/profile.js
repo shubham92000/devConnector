c69f45d32ecf252e91c5a788e72ce3af34ac0f91
shubham92000 <sbhsingh616@gmail.com>
2021-11-27 17:24:51 +0530

    added initial routes

---
+++ routes/api/profile.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+// @route   GET api/profiles
+// @desc    Test Route 
+// @access  Public
+router.get('/', (req, res) => {
+  res.send('Profile profile');
+});
+
+module.exports = router;
