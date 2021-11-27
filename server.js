9d522f16f4ede14d83ae1847a2c16284491d6479
shubham92000 <sbhsingh616@gmail.com>
2021-11-27 18:10:11 +0530

    added register user in db

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 // connect to db
 connectDB();
 
+// init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => {
   res.send('API runnning...');
 });
