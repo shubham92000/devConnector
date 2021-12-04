3f858ff04b09afb2c32c4941495ee9a095b81f9c
shubham92000 <sbhsingh616@gmail.com>
2021-12-04 09:37:03 +0530

    added createProfile action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -22,3 +22,86 @@ export const getCurrentProfile = () => async (dispatch) => {
     });
   }
 };
+
+// create / update profile
+export const createProfile =
+  (formData, navigate, edit = false) =>
+  async (dispatch) => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+
+      const res = await axios.post('/api/profile', formData, config);
+
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      });
+
+      dispatch(
+        setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success')
+      );
+
+      if (!edit) {
+        // <Navigate to="/dashboard" replace={true} />;
+        navigate('/dashboard', { replace: true });
+      }
+    } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+      }
+
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: {
+          msg: err.response.statusText,
+          status: err.response.status,
+        },
+      });
+    }
+  };
+
+// // create / update profile   v5
+// export const createProfile =
+//   (formData, history, edit = false) =>
+//   async (dispatch) => {
+//     try {
+//       const config = {
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//       };
+
+//       const res = await axios.post('/api/profile', formData, config);
+
+//       dispatch({
+//         type: GET_PROFILE,
+//         payload: res.data,
+//       });
+
+//       dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created'));
+
+//       if (!edit) {
+//         history.push('/dashboard');
+//       }
+//     } catch (err) {
+//       const errors = err.response.data.errors;
+
+//       if (errors) {
+//         errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+//       }
+
+//       dispatch({
+//         type: PROFILE_ERROR,
+//         payload: {
+//           msg: err.response.statusText,
+//           status: err.response.status,
+//         },
+//       });
+//     }
+//   };
--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,12 @@
 import React, { Fragment, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile';
 
-const CreateProfile = (props) => {
+const CreateProfile = ({ createProfile }) => {
+  let navigate = useNavigate();
+
   const [formData, setFormData] = useState({
     company: '',
     website: '',
@@ -38,6 +42,11 @@ const CreateProfile = (props) => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createProfile(formData, navigate);
+  };
+
   return (
     <div className="container">
       <h1 className="large text-primary">Create Your Profile</h1>
@@ -46,7 +55,7 @@ const CreateProfile = (props) => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={(e) => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -213,6 +222,8 @@ const CreateProfile = (props) => {
   );
 };
 
-CreateProfile.propTypes = {};
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
+};
 
-export default CreateProfile;
+export default connect(null, { createProfile })(CreateProfile);
