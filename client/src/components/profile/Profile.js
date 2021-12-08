dfea964983a57275147124f8dfb81cf3c75d2f36
shubham92000 <sbhsingh616@gmail.com>
2021-12-08 23:24:22 +0530

    added about section in ProfileAbout

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,7 +4,9 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 import { useParams } from 'react-router';
+import ProfileTop from './ProfileTop';
 import { Link } from 'react-router-dom';
+import ProfileAbout from './ProfileAbout';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
   let params = useParams();
@@ -24,12 +26,16 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
             Back To Profiles
           </Link>
           {auth.isAuthenticated &&
-            auth.loading == false &&
+            auth.loading === false &&
             auth.user._id === params.id && (
               <Link to="/edit-profile" className="btn btn-dark">
                 Edit Profile
               </Link>
             )}
+          <div className="profile-grid my-1">
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+          </div>
         </Fragment>
       )}
     </div>
